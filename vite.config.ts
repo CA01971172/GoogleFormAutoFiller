ee9b489e46197ebaf642d23853bc6eb95d210367
†村神† <CA01971172@st.kawahara.ac.jp>
2023-10-31 09:59:50 +0900

    implement divided export

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import { resolve } from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        app: resolve(root, "app", "main.ts"),
+        popup: resolve(root, "popup", "index.html"),
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      },
+    }
+  },
   plugins: [react()],
 })
