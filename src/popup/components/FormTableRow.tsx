44cc83646a60733c0d53a619721f68185d8688d1
†村神† <CA01971172@st.kawahara.ac.jp>
2023-11-01 12:13:48 +0900

    Implement FormTableRow

--- src/popup/components/FormTableRow.tsx
+++ src/popup/components/FormTableRow.tsx
@@ -1,18 +1,34 @@
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { FormInfo } from "../utils/fetchStorage";
 
 export default function FormTableRow({data}: {data: FormInfo}) {
     return (
         <>
             <Grid item xs={5}>
-                <div>{data.label}</div>
+                <TextField
+                    variant="standard"
+                    fullWidth
+                    label="指定のラベル"
+                    placeholder="指定するラベル名を入力してください"
+                    multiline
+                />
             </Grid>
             <Grid item xs={5}>
-                <div>{data.text}</div>
+                <TextField
+                    variant="standard"
+                    fullWidth
+                    label="入力する内容"
+                    placeholder="指定のラベルに入力する内容を入力してください"
+                    multiline
+                />
             </Grid>
             <Grid item xs={2}>
-                <div>あ</div>
+            <IconButton aria-label="delete">
+                <DeleteIcon />
+            </IconButton>
             </Grid>
         </>
     )
