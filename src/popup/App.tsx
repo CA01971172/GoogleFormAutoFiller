0a439f42701071d41f61bf43993b185053236f63
†村神† <CA01971172@st.kawahara.ac.jp>
2023-11-02 09:50:16 +0900

    Set primary color

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,9 +1,20 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import FormTable from "./components/FormTable";
 
 export default function App() {
+    // MUIのスタイル
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#673ab7",
+            }
+        }  
+    });
+
+
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <FormTable/>
-        </>
+        </ThemeProvider>
     )
 }
--- src/popup/components/FormTableRow.tsx
+++ src/popup/components/FormTableRow.tsx
@@ -15,24 +15,26 @@ export default function FormTableRow({data, index}: {data: FormInfo, index: numb
         <>
             <Grid item xs={5}>
                 <TextField
-                    variant="standard"
-                    fullWidth
                     label="指定のラベル"
                     placeholder="指定するラベル名を入力"
                     multiline
                     value={data.label}
                     onChange={(event) => rewriteForm(index, "label", event.target.value)}
+                    variant="standard"
+                    fullWidth
+                    color="primary"
                 />
             </Grid>
             <Grid item xs={5}>
                 <TextField
-                    variant="standard"
-                    fullWidth
                     label="入力する内容"
                     placeholder="指定のラベルに入力する内容を入力"
                     multiline
                     value={data.text}
                     onChange={(event) => rewriteForm(index, "text", event.target.value)}
+                    variant="standard"
+                    fullWidth
+                    color="primary"
                 />
             </Grid>
             <Grid item xs={2}>
